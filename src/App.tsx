e8c5636965ec51ee1983f11ea85851fdf70777fa
Nourhan Reda <nourreda276@gmail.com>
2026-02-05 20:24:21 +0200

    create repo

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+	return (
+		<div className="min-h-screen flex flex-col bg-black text-white/95 items-center justify-center text-2xl font-bold text-center">
+			<p>Hello</p>
+		</div>
+	);
+};
+
+export default App;
